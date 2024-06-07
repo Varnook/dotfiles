8934fd58c257c7b220b28f65a4b3d81f5da2c7e3
kin-over-sear <juan.mnul@gmail.com>
2024-06-07 12:24:17 -0300

    Add md utils and keybinds

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,4 +1,41 @@
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-    pattern = { "*.heex" }, 
-    command = "set syntax=html" 
+local current_buf = 0
+
+function create_header_link()
+    local line = vim.api.nvim_get_current_line()
+    if string.find(line, "#") == nil then
+        return
+    end
+
+    -- Trim spaces around the string and # at the beginning
+    local l = 1
+    while string.find(" #", string.sub(line, l, l)) ~= nil do
+        l = l + 1
+    end
+
+    local r = string.len(line)
+    while string.sub(line, r, r) == ' ' do
+        r = r - 1
+    end
+
+    identifier = string.sub(line, l, r)
+    identifier = identifier:gsub(" ", "_")
+
+    local new_line = table.concat({line, ' <a id="', identifier, '"> </a>'}, "")
+    vim.api.nvim_set_current_line(new_line)
+end
+
+local function markdown_keybinds()
+    local keybind_opts = { noremap = true, silent = true }
+    vim.api.nvim_buf_set_keymap(current_buf, 'n', '<space>hl', ':lua create_header_link()<CR>', keybind_opts)
+end
+
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+    pattern = {"*.md"},
+    callback = markdown_keybinds
 })
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+    pattern = { "*.heex" },
+    command = "set syntax=html"
+})
+
--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -3,17 +3,22 @@ local opts = { noremap = true, silent = true }
 
 keymap("n", "<c-l>", ":vertical resize +1<CR>", opts)
 keymap("n", "<c-h>", ":vertical resize -1<CR>", opts)
-keymap("n", "<Space>d", ":lua vim.diagnostic.open_float()<CR>", opts)
-keymap("n", "<Space>D", ":lua vim.diagnostic.open_float({scope='l'})<CR>", opts)
-keymap("n", "<Space>c", ":lua vim.cmd('pclose')<CR>", opts)
- 
+keymap("n", "<space>d", ":lua vim.diagnostic.open_float()<CR>", opts)
+keymap("n", "<space>D", ":lua vim.diagnostic.open_float({scope='l'})<CR>", opts)
+keymap("n", "<space>c", ":lua vim.cmd('pclose')<CR>", opts)
+keymap("n", "<space>sc", ":setlocal spell spelllang=en_us<CR>", opts)
+keymap("n", "<space>SC", ":setlocal nospell<CR>", opts)
+keymap("n", "<space>rn", ":setlocal relativenumber<CR>", opts)
+keymap("n", "<space>RN", ":setlocal norelativenumber<CR>", opts)
+
+keymap("n", "<c-s>", ":echo synIDattr(synID(line('.'), col('.'), 1), 'name')<CR>", opts)
+
 keymap("i", "{<cr>", "{<cr>}<c-o>O", opts)
 keymap("i", "[<cr>", "[<cr>]<c-o>O", opts)
 keymap("i", "(<cr>", "(<cr>)<c-o>O", opts)
 keymap("i", "{<Space>", "{", opts)
 keymap("i", "[<Space>", "[", opts)
 keymap("i", "(<Space>", "(", opts)
-
 
 keymap("v", "<s-k>", ":m .-2<CR>==<CR>k<s-v>", opts)
 keymap("v", "<s-j>", ":m .+1<CR>==<s-v>", opts)
